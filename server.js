4a4a3a07e91f2502c654eaa2740a35ebdb3f709a
Tumo M <tumomasire@gmail.com>
2020-01-13 15:14:14 +0200

    Heroku 2

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 app.use(cors());
 
-const port = process.env.PORT || 4000
+const PORT = process.env.PORT || 4000
 
 
 app.get("/", (req, res) => { root.handleRoot(req,res,knex)})
@@ -37,7 +37,7 @@ app.put('/image', (req, res) => { image.handleImage(req,res,knex)})
 
 app.post('/imageurl', (req, res) => { image.handleApiCall(req,res)})
 
-app.listen(port, () => {
-    console.log(`Server running on: `, port);
+app.listen(PORT, () => {
+    console.log(`Server running on: `, PORT);
 
 })
